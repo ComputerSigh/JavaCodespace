0002793156b255f706f4061013a4fa4da9abf8fd
Kurt Meyer <kmeyer@thacher.org>
2023-05-17 23:50:21 +0000

    added ActionListener

--- AWTExample.java
+++ AWTExample.java
@@ -1,15 +1,24 @@
 import java.awt.*;
 import java.awt.event.*;
 
+import javax.swing.*;
+
 public class AWTExample extends Frame {
+   
+  ButtonHandler bHandler = new ButtonHandler();
+  JButton btn = new JButton("Close");
+
   public AWTExample() {
-    Button btn = new Button("Button");
+	setSize(400, 100);
+	setLocation(600, 300);
+	
     btn.setBounds(50, 50, 50, 50);
+    btn.addActionListener(bHandler);
     add(btn);
-    setSize(350, 150);
     setTitle("This is my First AWT example");
     setLayout(new FlowLayout());
     setVisible(true);
+    
     addWindowListener(new WindowAdapter() {
         public void windowClosing(WindowEvent we) {
             dispose();
@@ -17,7 +26,17 @@ public class AWTExample extends Frame {
     });
   }
 
+   class ButtonHandler implements ActionListener {
+
+	  // handle button events
+	  public void actionPerformed(ActionEvent event) {
+
+		 if (event.getSource() == btn) 
+			System.exit(0);;
+	  } 
+  }
+
   public static void main(String args[]){
     new AWTExample();
-  }
+  } 
 }
