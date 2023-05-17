66201028d6fc42fed7eb9d51c264709d7b223421
Kurt Meyer <kmeyer@thacher.org>
2023-05-17 04:22:18 +0000

    Change Filename

--- FractalDriver.java
+++ FractalDriver.java
@@ -1,4 +1,4 @@
-public class HelloCodespace {
+public class FractalDriver {
   public static void main(String[] args) {
 	  FractalWindow game = new FractalWindow(600, 500);
 	  game.setVisible(true);
