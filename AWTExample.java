4cd4ad2f9c072eec3d7c7dd17efdd108f35469a6
Kurt Meyer <kmeyer@thacher.org>
2023-05-17 23:22:22 +0000

    minor edit

--- AWTExample.java
+++ AWTExample.java
@@ -6,7 +6,7 @@ public class AWTExample extends Frame {
     Button btn = new Button("Button");
     btn.setBounds(50, 50, 50, 50);
     add(btn);
-    setSize(150, 150);
+    setSize(350, 150);
     setTitle("This is my First AWT example");
     setLayout(new FlowLayout());
     setVisible(true);
